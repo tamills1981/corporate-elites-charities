3ffe0a91651d1ee613227a5840b45dac4d44a76d
tamills1981 <tamills1981@gmail.com>
2024-02-12 15:20:59 +0000

    pagination and a few tweaks

--- 2. companies house directors scrape.py
+++ 2. companies house directors scrape.py
@@ -15,6 +15,8 @@ companies = get_companies(DATA_DIR)
 #Create list from Series to populate API queries
 company_nos = companies.company_no.tolist()
 
+company_nos = company_nos[0:50]
+
 #Upload API key from main folder
 api_key = open(f"{DATA_DIR}/api_key.txt", "r")
 api_key = (api_key.read())
@@ -22,43 +24,53 @@ api_key = (api_key.read())
 #URL for company officers list
 base_url = "https://api.company-information.service.gov.uk/company/"
 
+#Empty lists and dictionary to populate 
 dir_list =[]
+dir_data_list = []
 status_codes = {}
-dir_dump_list = []
 
 request_number = 0
+items_per_page = 35
 
-for item in tqdm(company_nos):
-    if request_number > 1100:
-        print("sleeping")
-        time.sleep(300)
-        request_number = 0
-    else: pass
-    response = requests.get(f"{base_url}{item}/officers",auth=(api_key,''))
-    request_number = request_number + 1 
-    status_codes[item] = {}
-    status_codes[item]['status_code'] = response.status_code
-    status_codes[item]['timestamp'] = str(datetime.now())
-    status_codes[item]['request_number'] = request_number 
-    if response.status_code == 429:
-        print("429_sleeping")
-        time.sleep(300)
-        request_number = 0
-        continue
-    else: pass
-    if response.status_code != 200:
-        continue
-    else: pass
-    json_search_result = response.text
-    data = json.JSONDecoder().decode(json_search_result)
-    for item in data['items']:
-        dir_dump_list.append(item)
-    for item in data['items']:
-        dir_list.append(item['links']['officer']['appointments'])
+for company_no in tqdm(company_nos):
+    start_index = 0
+    total_results = float('inf')
 
-dir_info = pd.json_normalize(dir_dump_list)
+    while start_index < total_results:
+        if request_number > 1100:
+            print("sleeping")
+            time.sleep(300)
+            request_number = 0
+        else:
+            pass
+
+        response = requests.get(f"{base_url}{company_no}/officers?start_index={start_index}&items_per_page={items_per_page}", auth=(api_key, ''))
+        request_number += 1
+
+        status_codes[company_no] = {
+            'page_number': start_index,
+            'status_code': response.status_code,
+            'timestamp': str(datetime.now()),
+            'request_number': request_number
+        }
+
+        if response.status_code != 200:
+            break
+
+        data = json.loads(response.text)
+        total_results = data['total_results']
+
+        for item in data['items']:
+            dir_data_list.append(item)
+            dir_list.append(item['links']['officer']['appointments'])
+
+        start_index += items_per_page
+
+dir_info = pd.json_normalize(dir_data_list)
 dir_search_statuses = pd.DataFrame.from_dict(status_codes)
 dir_search_statuses = dir_search_statuses.T
+
+print(dir_search_statuses['status_code'].value_counts())
 
 #Export director info, list and search statuses
 dir_info.to_csv(f'{DATA_DIR}/outputs/dir_info.csv', index=False)

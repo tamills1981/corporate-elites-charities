70a7644b462c7e95ad5797bc0781384f3c355a3e
tamills1981 <tamills1981@gmail.com>
2024-03-05 14:18:50 +0000

    integrated charities and oxbridge variables into fifth script in pipeline

--- 5. corporate registered charities.py
+++ 5. corporate registered charities.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from utils import PROJECT_DIR
-from getters import get_charities, get_royal_patronage, get_corporate_elite_appointments
+from getters import get_charities, get_royal_patronage, get_corporate_elite_appointments, get_private_school_charities, get_oxbridge_charities
 
 DATA_DIR = f'{PROJECT_DIR}/'
 
@@ -8,6 +8,8 @@ DATA_DIR = f'{PROJECT_DIR}/'
 charity_data = get_charities(DATA_DIR)
 royal_patronage = get_royal_patronage(DATA_DIR)
 corporate_elite_appointments = get_corporate_elite_appointments(DATA_DIR)
+private_school_charities = get_private_school_charities(DATA_DIR)
+oxbridge_charities = get_oxbridge_charities(DATA_DIR)
 
 #Drop charities registered after 2023 
 filt = (charity_data['date_of_registration'] < '2023')
@@ -28,8 +30,14 @@ CRCs = CRCs[['organisation_number', 'registered_charity_number', 'charity_compan
              'charity_contact_phone', 'charity_contact_email', 'charity_contact_web', 'charity_gift_aid', 
              'charity_has_land']]
 
-#Create new royal patronage boolean variable 
-CRCs['royal_patronage'] = CRCs['organisation_number'].isin(royal_patronage['registered_charity_number']) | CRCs['registered_charity_number'].isin(royal_patronage['registered_charity_number'])
+#Create royal patronage boolean variable 
+CRCs['royal_patronage'] = CRCs['organisation_number'].isin(royal_patronage['registered_charity_number'])
+
+#Create private school charity variable
+CRCs['private_school'] = CRCs['organisation_number'].isin(private_school_charities['organisation_number'])
+
+#Create Oxbridge charity variable
+CRCs['oxbridge'] = CRCs['organisation_number'].isin(oxbridge_charities['organisation_number'])
 
 #Create new charity age variable
 enddate = pd.to_datetime('31-12-2022')
@@ -37,7 +45,7 @@ def calculate_age(date_of_reg):
     return enddate.year - date_of_reg.year
 CRCs['charity_age'] = CRCs['date_of_registration'].apply(calculate_age)
 
-#Create corporate elite trustee boolean variablea
+#Create corporate elite trustee boolean variable
 CRCs['corporate_elite_trustee'] = CRCs['charity_company_registration_number'].isin(corporate_elite_appointments['appointed_to.company_number'])
 
 #Export the data as a csv
